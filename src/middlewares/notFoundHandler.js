e8a27c1c4118e396cff69002d69d59171cccde9d
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-12-18 15:36:27 +0200

    fix task

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,5 @@
-export const notFoundHandler = (req, res) => {
-  res.status(404).json({
-    message: `${req.url} not found`,
-  });
+import createHttpError from 'http-errors';
+
+export const notFoundHandler = (req, res, next) => {
+  throw new createHttpError(404, 'Route not found');
 };
