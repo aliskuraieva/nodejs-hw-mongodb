e11cb7cfbc43845cf3736737176f10ada0292b92
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-12-25 10:44:53 +0200

    fix task

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,12 @@
+import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
-import createError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
+
   if (!isValidObjectId(contactId)) {
-    return next(createError(404, `${contactId} not valid id`));
+    next(createHttpError(400, 'Bad Id'));
   }
+
   next();
 };
