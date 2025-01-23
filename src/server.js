a75dba2097cca6b9d3fe58c558bf9262f60c5b0f
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-01-23 14:39:26 +0200

    fixed

--- src/server.js
+++ src/server.js
@@ -25,14 +25,18 @@ export const setupServer = () => {
     }),
   );
 
+  
+
   app.use(router);
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
 
   app.use('*', errorHandler);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+  
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
